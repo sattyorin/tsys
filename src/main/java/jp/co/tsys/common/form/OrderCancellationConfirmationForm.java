03a57dc964c71b075231ffad26bad3397a636a2b
s-ozeki <hoge@hoge.com>
2023-09-05 15:58:39 +0900

    fukamizu: added OrderCancellationConfirmationForm

---
+++ src/main/java/jp/co/tsys/common/form/OrderCancellationConfirmationForm.java
@@ -0,0 +1,27 @@
+/**
+ * OrderCancellationConfirmationForm.java
+ */
+
+package jp.co.tsys.common.form;
+
+import java.util.List;
+
+import jp.co.tsys.common.entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author FLM
+ * @version 1.0.0
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCancellationConfirmationForm {
+
+	private List<Order> orders;
+
+}
