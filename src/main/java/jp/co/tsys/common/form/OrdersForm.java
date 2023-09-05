5f38d9052ed644c39cd38ae3efd33e3649c48877
s-ozeki <hoge@hoge.com>
2023-09-05 15:45:30 +0900

    ohota: added OrdersForm and ShoppingCartForm

---
+++ src/main/java/jp/co/tsys/common/form/OrdersForm.java
@@ -0,0 +1,44 @@
+package jp.co.tsys.common.form;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import jp.co.tsys.common.entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author FLM
+ * @version 1.0.0
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrdersForm implements Serializable {
+
+	@NotBlank
+	@Size(max = 6)
+	private String memberCode;
+
+	@NotBlank
+	private String orderNo;
+
+	@NotBlank
+	private String orderDate;
+
+	@NotBlank
+	private int orderTotal;
+
+	@NotBlank
+	private String payment;
+
+	@NotBlank
+	private List<Order> orders;
+
+}
