e6fc196bb077b4189b2796b77e48c45799e440be
s-ozeki <hoge@hoge.com>
2023-09-05 16:03:52 +0900

    soutaro: added SearchOrderForm

---
+++ src/main/java/jp/co/tsys/common/form/SearchOrderForm.java
@@ -0,0 +1,29 @@
+/**
+ * SearchOrderForm.java
+ */
+
+package jp.co.tsys.common.form;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author FLM
+ * @version 1.0.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchOrderForm implements Serializable {
+	@NotBlank
+	@Size(max = 6)
+	private String memberCode;
+
+}
