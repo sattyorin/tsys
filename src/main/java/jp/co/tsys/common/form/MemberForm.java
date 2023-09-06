c8778cbcd273b422d82340ef778c597feda3ff52
s-ozeki <hoge@hoge.com>
2023-09-06 13:34:49 +0900

    ichii: added MemberForm

---
+++ src/main/java/jp/co/tsys/common/form/MemberForm.java
@@ -0,0 +1,63 @@
+/**
+ * NonRoleEmployeeMemberForm.java
+ */
+
+package jp.co.tsys.common.form;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author FLM
+ * @version 1.0.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberForm implements Serializable {
+
+	@NotBlank
+	private String role;
+
+	@NotBlank
+	private String firstName;
+
+	@NotBlank
+	private String lastName;
+
+	@NotBlank
+	@Size(min = 8, max = 15)
+	private String password;
+
+	@NotBlank
+	@Size(min = 8, max = 15)
+	private String confirmpassword;
+
+	@NotBlank
+	@Email
+	private String mail;
+
+	@NotBlank
+	@Pattern(regexp = "^\\d{3}\\-?\\d{4}$")
+	private String zipCode;
+
+	@NotBlank
+	private String prefecture;
+
+	@NotBlank
+	private String address;
+
+	@NotBlank
+	@Pattern(regexp = "0\\d{1,4}-\\d{1,4}-\\d{4}")
+	private String tel;
+
+}
