dad28aed385b1d839469a4ebf6f670dbd707443c
s-ozeki <hoge@hoge.com>
2023-09-06 09:57:22 +0900

    ren: added HotelMasterMapper

---
+++ src/main/java/jp/co/tsys/common/mapper/HotelMasterMapper.java
@@ -0,0 +1,25 @@
+/**
+ * HotelMasterMapper.java
+ *
+ * All Rights Reserved, Copyright(c) Fujitsu Learning Media Limited
+ */
+
+package jp.co.tsys.common.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import jp.co.tsys.common.entity.Hotel;
+
+/**
+ *
+ * @author FLM
+ * @version 1.0.0
+ */
+@Mapper
+public interface HotelMasterMapper {
+	public List<Hotel> findHotel(@Param("hotelCode") String hotelCode,
+			@Param("hotelName") String hotelName);
+}
