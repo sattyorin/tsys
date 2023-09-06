0d38be7e4bdbc510ec22fc4189e235a8e3dd4032
s-ozeki <hoge@hoge.com>
2023-09-06 15:40:23 +0900

    shino: added FindAllMemberForm

---
+++ src/main/java/jp/co/tsys/common/form/FindAllMemberForm.java
@@ -0,0 +1,40 @@
+/**
+ * FindAllMemberForm.java
+ */
+
+package jp.co.tsys.common.form;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author FLM
+ * @version 1.0.0
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class FindAllMemberForm implements Serializable {
+
+	@NotBlank
+	private String role;
+
+	private String name;
+
+	@Pattern(regexp = "0\\d{1,4}-\\d{1,4}-\\d{4}")
+	private String tel;
+
+	@Email
+	private String mail;
+
+}
