c62554c414787f654e95be943aa0587d0f6289ba
s-ozeki <hoge@hoge.com>
2023-09-07 10:43:01 +0900

    soutaro: modified root path and contents

--- src/main/java/jp/co/tsys/common/control/MemberOrdersFindController.java
+++ src/main/java/jp/co/tsys/common/control/MemberOrdersFindController.java
@@ -1,7 +1,5 @@
 /**
  * MemberOrdersFindController.java
- *
- * All Rights Reserved, Copyright(c) Fujitsu Learning Media Limited
  */
 
 package jp.co.tsys.common.control;
@@ -14,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jp.co.tsys.common.entity.Member;
 import jp.co.tsys.common.entity.Order;
@@ -25,6 +24,7 @@ import jp.co.tsys.common.service.MemberOrdersFindService;
  * @author FLM
  * @version 1.0.0
  */
+@SessionAttributes(types = Member.class)
 @Controller
 @RequestMapping("/order")
 public class MemberOrdersFindController {
@@ -32,36 +32,53 @@ public class MemberOrdersFindController {
 	private MemberOrdersFindService service;
 
 	// 顧客情報検索画面に遷移する。
-	@RequestMapping("/find")
+	@RequestMapping("order/find")
 	public String findMemberOrders(Model model) {
 		return "V203_01MemberCodeFindView";
 	}
 
 	// 顧客情報検索画面の検索ボタンに対応するメソッド
 	// 受注一覧画面に遷移する。
-	@RequestMapping("/history")
-	public String findMemberOrders(@Validated MemberCodeForm form, BindingResult result, Model model) {
-		// 入力チェック
-		if (result.hasErrors()) {
-			// 受注一覧画面
-			return "V203_01MemberCodeFindView";
+	@RequestMapping("order/history")
+	public String findMemberOrders(@Validated MemberCodeForm memberCodeForm,
+			BindingResult result, Model model) {
+		// 初期値
+		Member member;
+		List<Order> listCurrentOrder;
+		List<Order> listPastOrder;
+		// セッションから情報取得
+		Member loginMember = (Member) model.getAttribute("loginmember");
+		// member情報から従業員と顧客を条件分岐
+		if ("Employee".equals(loginMember.getRole())) {
+			// 入力チェック
+			if (result.hasErrors()) {
+				// 受注一覧画面
+				return "V203_01MemberCodeFindView";
+			}
+
+			// Service で各メソッドを呼び出し
+			// 戻り値Memberオブジェクトを取得する
+			member = service.findMember(memberCodeForm.getMemberCode());
+			listCurrentOrder = service
+					.findCurrentOrder(memberCodeForm.getMemberCode());
+			listPastOrder = service
+					.findPastOrder(memberCodeForm.getMemberCode());
+		} else {
+			// Service で各メソッドを呼び出し
+			// 戻り値Memberオブジェクトを取得する
+			member = service.findMember(loginMember.getMemberCode());
+			listCurrentOrder = service
+					.findCurrentOrder(loginMember.getMemberCode());
+			listPastOrder = service.findPastOrder(loginMember.getMemberCode());
 		}
-
-		// Service で各メソッドを呼び出し
-		// 戻り値Memberオブジェクトを取得する
-		Member member = service.findMember(form.getMemberCode());
-		List<Order> listCurrentOrder = service.findCurrentOrder(form.getMemberCode());
-		List<Order> listPastOrder = service.findPastOrder(form.getMemberCode());
-
-
 		// memberオブジェクトをキー名"member"でModelに格納
 		model.addAttribute("member", member);
 		// Listオブジェクトをキー名"listPastOrder"でModelに格納
 		model.addAttribute("listPastOrder", listPastOrder);
 		// Listオブジェクトをキー名"listCurrentOrder"でModelに格納
 		model.addAttribute("listCurrentOrder", listCurrentOrder);
-
 		return "V203_02OrderHistory";
+
 	}
 
 	// 受注履歴を削除して確認画面に遷移。
