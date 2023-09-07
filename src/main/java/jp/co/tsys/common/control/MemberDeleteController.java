97219149a0864e499c1388e7fe9f8690e66c3ffa
s-ozeki <hoge@hoge.com>
2023-09-07 13:50:44 +0900

    suguru: modified MemberDeleteController

--- src/main/java/jp/co/tsys/common/control/MemberDeleteController.java
+++ src/main/java/jp/co/tsys/common/control/MemberDeleteController.java
@@ -8,15 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jp.co.tsys.common.entity.Member;
 import jp.co.tsys.common.exception.BusinessException;
-import jp.co.tsys.common.form.MemberCodeForm;
 import jp.co.tsys.common.service.MemberDeleteService;
 
 /**
@@ -26,54 +24,43 @@ import jp.co.tsys.common.service.MemberDeleteService;
  */
 @Controller
 @RequestMapping("/member/delete")
+@SessionAttributes(names = {"loginmember", "member"})
 public class MemberDeleteController {
 
 	@Autowired
 	private MemberDeleteService service;
 
-	@RequestMapping("/find")
-	public String findMember(@Validated MemberCodeForm form,
-			BindingResult result, Model model) {
-		// 入力チェック
-		if (result.hasErrors()) {
-
-			// メンバー検索画面（/member/find/search）を返却する
-			return "/delete/MemberFindView";
-		}
-
-		// ServiceのfindMemberメソッドを呼び出し
-		// 戻り値のmemberオブジェクトを取得する
-		Member member = service.findMember(form.getMemberCode());
-
-		// Memberオブジェクトをキー名"member"でModelに格納
-		model.addAttribute("member", member);
-
-		return "/delete/MemberDetailView";
-	}
-
 	// 従業員がメンバーを削除する際の「確認」に対応するメソッド
 	// 顧客が退会する際の「確認」に対応するメソッド
 	@RequestMapping("/confirm")
 	public String deleteMemberConfirm(String memberCode) {
-
-		return "/delete/V902_01MemberDeleteConfirmView";
+		return "member/delete/member_delete_confirm";
 
 	}
 
 	// 従業員がメンバーを削除する際の「確定」に対応するメソッド
 	// 顧客が退会する際の「確定」に対応するメソッド
-	@RequestMapping("/member/delete/result")
-	public String deleteMember(String memberCode) {
+	@RequestMapping("/result")
+	public String deleteMember(Model model) {
+		// 削除が失敗した際のエラーをキャッチする
+		try {
+			Member member = (Member) model.getAttribute("member");
+			service.deleteMember(member.getMemberCode());
+		} catch (BusinessException e) {
+			// エラーメッセージをキー名"message"でModelに格納
+			model.addAttribute("message", e.getMessage());
+			return "member/delete/member_delete_final_confirm";
+		}
 
-		return "/delete/V902_02MemberDeleteResultView";
+		return "member/delete/member_delete_result";
 
 	}
 
 	// 顧客が退会する際の「最終確認」に対応するメソッド
-	@RequestMapping("finalconfirm")
+	@RequestMapping("/finalconfirm")
 	public String deleteCustomerFinalConfirm(String memberCode) {
 
-		return "/delete/V902_03MemberDeleteFinalConfirmView";
+		return "member/delete/member_delete_final_confirm";
 
 	}
 
@@ -83,10 +70,7 @@ public class MemberDeleteController {
 		// エラーメッセージをキー名"message"でModelに格納
 		model.addAttribute("message", e.getMessage());
 
-		// フォームオブジェクトをキー名"MemberCodeForm"でModelに格納
-		// model.addAttribute("MemberCodeForm", new MemberCodeForm());
-
-		return "/delete/V902_01MemberDeleteConfirmView"; // ここ確認する
+		return "member/delete/member_delete_confirm"; // ここ確認する
 	}
 
 }
--- src/main/java/jp/co/tsys/common/service/MemberDeleteService.java
+++ src/main/java/jp/co/tsys/common/service/MemberDeleteService.java
@@ -1,15 +1,10 @@
 /**
  * MemberDeleteService.java
- *
- * All Rights Reserved, Copyright(c) Fujitsu Learning Media Limited
  */
 
 package jp.co.tsys.common.service;
 
-
-import jp.co.tsys.common.entity.Member;
 import jp.co.tsys.common.exception.BusinessException;
-
 
 /**
  *
@@ -17,8 +12,6 @@ import jp.co.tsys.common.exception.BusinessException;
  * @version 1.0.0
  */
 public interface MemberDeleteService {
-
-	public Member findMember(String memberCode) throws BusinessException;
 
 	public boolean deleteMember(String memberCode) throws BusinessException;
 
