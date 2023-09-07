33afb84486bb9a66e297955fcd41d4e64e9ec572
s-ozeki <hoge@hoge.com>
2023-09-07 15:23:00 +0900

    matsuo: added html and TopMenuController

---
+++ src/main/java/jp/co/tsys/common/control/TopMenuController.java
@@ -0,0 +1,73 @@
+/**
+ * TopMenuController.java
+ */
+
+package jp.co.tsys.common.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * トップメニュー画面遷移Controller
+ * 
+ * @author FLM
+ * @version 1.0.0
+ */
+
+@Controller
+@RequestMapping("/topmenu")
+public class TopMenuController {
+	/**
+	 * 顧客用トップメニュー画面の【ホテル予約】に対応するHandlerメソッド マッピングするURL：/customerhotelreserve
+	 * return 顧客ホテル予約画面(findHotel.html)
+	 */
+	@RequestMapping("/customerhotelreserve")
+	public String hotelReserve() {
+		return "/findHotel";
+	}
+
+	/**
+	 * 顧客用トップメニュー画面の【予約履歴】に対応するHandlerメソッド マッピングするURL：/reserveHistory return
+	 * 予約履歴画面(V0203_02OrderHistory.html)
+	 */
+	@RequestMapping("/reservehistory")
+	public String reserveHistory() {
+		return "/V0203_02OrderHistory";
+	}
+
+	/**
+	 * 顧客用トップメニュー画面の【お客様情報 変更・退会】に対応するHandlerメソッド マッピングするURL：/changemember
+	 * return お客様情報詳細画面(/member/find/retrive.html)
+	 */
+	@RequestMapping("/changemember")
+	public String changeMember() {
+		return "/member/find/retrive";
+	}
+
+	/**
+	 * 従業員用トップメニュー画面の【ホテル予約】に対応するHandlerメソッド マッピングするURL：/emphotelreserve return
+	 * 従業員ホテル予約画面(/hotelreservation/sendemployeemenu)
+	 */
+	@RequestMapping("/emphotelreserve")
+	public String empHotelReserve() {
+		return "/hotelreservation/sendemployeemenu";
+	}
+
+	/**
+	 * 従業員用トップメニュー画面の【商品管理】に対応するHandlerメソッド マッピングするURL：/productmanage return
+	 * 商品管理画面(/UC501_regist/M0004_001ItemMgrMenu)
+	 */
+	@RequestMapping("/productmanage")
+	public String productManage() {
+		return "/UC501_regist/M0004_001ItemMgrMenu";
+	}
+
+	/**
+	 * 従業員用トップメニュー画面の【メンバー管理】に対応するHandlerメソッド マッピングするURL：/membermanage return
+	 * 商品管理画面(/member_mgr)
+	 */
+	@RequestMapping("/membermanage")
+	public String memberManage() {
+		return "/member_mgr";
+	}
+}
