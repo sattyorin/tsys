cf31121ca0defd4e73b741adef4e7b192489977e
s-ozeki <hoge@hoge.com>
2023-09-08 15:26:50 +0900

    ohota: added pymentcontroller

--- src/main/java/jp/co/tsys/common/control/PaymentController.java
+++ src/main/java/jp/co/tsys/common/control/PaymentController.java
@@ -1,39 +1,52 @@
 /**
+ *
  */
 package jp.co.tsys.common.control;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import jp.co.tsys.common.entity.Member;
 import jp.co.tsys.common.entity.Order;
 import jp.co.tsys.common.form.HotelDetailForm;
+import jp.co.tsys.common.form.OrdersForm;
 import jp.co.tsys.common.form.ShoppingCartForm;
+import jp.co.tsys.common.service.MemberFindService;
 import jp.co.tsys.common.service.PaymentService;
 
 @Controller
-@SessionAttributes(types = ShoppingCartForm.class)
+@SessionAttributes(types = {Member.class, HotelDetailForm.class,
+		ShoppingCartForm.class})
 @RequestMapping("/payment")
 public class PaymentController {
 
 	@Autowired
-	private PaymentService service;
+	private PaymentService paymentService;
+	@Autowired
+	private MemberFindService memberService;
 
+	// ホテルフォームとカートフォームがnullならエラーメッセージだす
 	@RequestMapping("/cart")
-	public String inputCart(@RequestParam HotelDetailForm hotelDetailForm,
-			ShoppingCartForm shoppingCartForm, BindingResult result,
-			Model model) {
+	public String inputCart(
+			@ModelAttribute("hotelDetailForm") HotelDetailForm hotelDetailForm,
+			@ModelAttribute("shoppingCartForm") ShoppingCartForm shoppingCartForm,
+			BindingResult result, Model model) {
 
 		if (result.hasErrors()) {
-			model.addAttribute("message", "error");
-			return "/payment/shopping_cart";
+
+			return "ホテル詳細画面";
 		}
 
 		//// 在庫チェック
@@ -46,48 +59,133 @@ public class PaymentController {
 		// return "/hotelfind/sendShoppingCart";
 		// }
 
-		List<Order> orderList;
+		List<Order> orderList = new ArrayList<>();
 
-		if (shoppingCartForm == null) {
-			orderList = new ArrayList<Order>();
-		} else {
+		if (shoppingCartForm.getOrders() != null) {
 			orderList = shoppingCartForm.getOrders();
+			// ＊shoppingCartFormがnullだと、例外でてしまう
 		}
+
+		// if (shoppingCartForm.getOrders() == null) {
+		// orderList = new ArrayList<Order>();
+		//// System.out.println("テスト");
+		// } else {
+		// orderList = shoppingCartForm.getOrders();
+		//// System.out.println("テスト２");
+		// }
+
+		// System.out.println(hotelDetailForm.getInputQuantity());
 
 		Order order = new Order();
 		order.setHotelItem(hotelDetailForm.getHotelItem());
 		order.setQuantity(Integer.parseInt(hotelDetailForm.getInputQuantity()));
+		order.setSubTotal(
+				order.getQuantity() * order.getHotelItem().getPrice());
 		orderList.add(order);
 
+		// 合計を計算
+		int sum = 0;
+		for (Order item : orderList) {
+			sum += item.getSubTotal();
+		}
+
+		// *orderList.forEach(e -> {
+		// sum += e.getSubTotal();
+		// });
+		// *map
+
 		shoppingCartForm.setOrders(orderList);
+		shoppingCartForm.setOrderTotal(sum);
+
 		model.addAttribute("shoppingCartForm", shoppingCartForm);
-		return "shopping_cart";
+
+		// for(Order item:shoppingCartForm.getOrders()){
+		// System.out.println("s"+item.getQuantity());
+		// }
+		return "/payment/shopping_cart";
 	}
 
-	// @RequestMapping("/confirmation")
-	// public String confirmeResult(@RequestParam ShoppingCartForm
-	// shoppingCartForm,
-	// BindingResult result, Model model) {
-	// return "order_confirmation";
-	// }
-	//
-	// @RequestMapping("/complition")
-	// public String commitResult(final OrdersForm ordersForm,ShoppingCartForm
-	// shoppingCartForm,
-	// BindingResult result, Model model,SessionStatus status) {
-	//
-	// Orders orders
-	// = new Orders(,shoppingCartForm.getOrders());
-	//
-	// // ServiceのregistMemberメソッドを呼び出す
-	// service.registMember(member);
-	//
-	// // セッションからフォームオブジェクトを削除
-	// status.setComplete();
-	//
-	// // 会員情報オブジェクトをキー名"member"でModelに格納
-	// model.addAttribute("member", member);
-	// return "order_commit";
-	// }
+	@RequestMapping("/confirmation")
+	public String confirmeResult(
+			@ModelAttribute("loginmember") Member loginmember,
+			ShoppingCartForm shoppingCartForm, BindingResult result,
+			Model model) {
 
+		OrdersForm ordersForm = new OrdersForm();
+
+		ordersForm.setOrders(shoppingCartForm.getOrders());
+		ordersForm.setOrderTotal(shoppingCartForm.getOrderTotal());
+
+		if (loginmember.getRole() == "Customer") {
+			ordersForm.setMemberCode(loginmember.getMemberCode());
+
+			// 顧客と従業員の名前衝突回避用にインスタンス作成
+			Member displayMemeber = new Member();
+			displayMemeber.setName(loginmember.getName());
+			displayMemeber.setZipCode(loginmember.getZipCode());
+			displayMemeber.setPrefecture(loginmember.getPrefecture());
+			displayMemeber.setAddress(loginmember.getAddress());
+			displayMemeber.setTel(loginmember.getTel());
+
+			model.addAttribute("displayMemeber", displayMemeber);
+		}
+
+		model.addAttribute("ordersForm", ordersForm);
+
+		return "/payment/order_confirmation";
+	}
+
+	@RequestMapping("/findMember")
+	public String findMember(@ModelAttribute OrdersForm ordersForm,
+			@ModelAttribute("shoppingCartForm") ShoppingCartForm shoppingCartForm,
+			BindingResult result, Model model) {
+
+		System.out.println(ordersForm.getMemberCode());
+
+		// サービスにメンバーコード投げてメンバーオブジェクトを取得
+		Member targetMember = memberService
+				.findMember(ordersForm.getMemberCode());
+		System.out.println(targetMember);
+
+		// 顧客と従業員の名前衝突回避用にインスタンス作成
+		Member displayMemeber = new Member();
+
+		ordersForm.setMemberCode(targetMember.getMemberCode());
+		displayMemeber.setName(targetMember.getName());
+		displayMemeber.setZipCode(targetMember.getZipCode());
+		displayMemeber.setPrefecture(targetMember.getPrefecture());
+		displayMemeber.setAddress(targetMember.getAddress());
+		displayMemeber.setTel(targetMember.getTel());
+
+		model.addAttribute("displayMemeber", displayMemeber);
+
+		return "/payment/order_confirmation";
+	}
+
+	@RequestMapping("/completion")
+	@Transactional
+	public String commitResult(OrdersForm ordersForm, BindingResult result,
+			Model model, SessionStatus status) {
+
+		// 現在日時を取得する
+		LocalDateTime date1 = LocalDateTime.now();
+		DateTimeFormatter dtformat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String fdate1 = dtformat.format(date1);
+		ordersForm.setOrderDate(fdate1);
+
+		// データ登録
+		paymentService.insertOrder(ordersForm);
+		String lastOrderNo = paymentService.getLastOrderNo();
+		ordersForm.setOrderNo(lastOrderNo);
+		paymentService.insertOrderDetail(ordersForm.getOrders(),
+				ordersForm.getOrderNo());
+
+		// 在庫減らす
+		paymentService.updateHotelStock(ordersForm.getOrders());
+
+		// カートの中身を破棄
+		model.addAttribute("shoppingCartForm", new ShoppingCartForm());
+
+		return "/payment/order_commit";
+	}
 }
