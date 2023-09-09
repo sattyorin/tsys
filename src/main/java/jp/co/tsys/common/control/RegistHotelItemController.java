70bd7f7b7dc2a981d069ea1fc1e07b20029e7a05
s-ozeki <hoge@hoge.com>
2023-09-10 00:27:44 +0900

    ren: added html of regist

--- src/main/java/jp/co/tsys/common/control/RegistHotelItemController.java
+++ src/main/java/jp/co/tsys/common/control/RegistHotelItemController.java
@@ -1,8 +1,13 @@
 /**
  * HotelRegistControll.java
+ *
+ * All Rights Reserved, Copyright(c) Fujitsu Learning Media Limited
  */
 
 package jp.co.tsys.common.control;
+
+import static jp.co.tsys.common.util.MessageList.BIZERR301;
+import static jp.co.tsys.common.util.MessageList.BIZERR302;
 
 import java.util.List;
 
@@ -27,90 +32,99 @@ import jp.co.tsys.common.service.RegistHotelService;
  * @author FLM
  * @version 1.0.0
  */
-
 @SessionAttributes(names = "lastHotelCode")
 @Controller
-@RequestMapping("hotelItem")
+@RequestMapping("/hotelitem")
 public class RegistHotelItemController {
 
 	@Autowired
 	private RegistHotelService service;
 
-	// 登録画面（検索画面）に遷移する
-	@RequestMapping("/hotelItem/register/entryHotelItemRegist")
-	public String entryHotelItemRegist(Model model, SessionStatus status) {
-
-		model.addAttribute("hotelFindForm", new FindHotelForm());
-
-		status.setComplete();
-		return "V0501_01HotelFindView";
-		// return null;
-
+	// 商品管理メニューに遷移（一番上）
+	@RequestMapping("/register/itemmenu")
+	public String itemMgrMenu(Model model) {
+		return "/item_menu";
 	}
 
-	//
-	//
-	// //商品管理メニューに遷移（一番上）
-	// @RequestMapping("/hotelItem/register/itemMgrMenu")
-	// public String itemMgrMenu(Model model) {
-	//
-	// return null;
-	// }
+	// 登録画面（検索画面）に遷移する
+	@RequestMapping("/register/entryhotelitemregist")
+	public String entryHotelItemRegist(Model model, SessionStatus status) {
+		model.addAttribute("findHotelForm", new FindHotelForm());
+
+		return "/hotelitem/regist/hotel_find";
+	}
 
 	// 検索ボタン押されたとき、検索画面にもう一度遷移
-	@RequestMapping("/hotelItem/register/findHotel")
+	@RequestMapping("/register/findhotel")
 	public String findHotel(@Validated FindHotelForm form, BindingResult result,
 			Model model) {
-
 		if (result.hasErrors()) {
-
-			// エラーメッセージは？？？
-
-			return "V0501_01HotelFindView";
+			// TODO(ren): addAttribute error message
+			return "/hotelitem/regist/hotel_find";
 		}
 
 		// code,name共に空白の場合は、何も表示せずにまた検索画面へ、どちらか入力の場合は＊にして検索
-		if (form.getHotelCode() == null && form.getHotelName() == null) {
-
-			model.addAttribute("message", "どちらか入力！！！！");
-			return "V0501_01HotelFindView";
-
+		if (form.getHotelCode().isEmpty() && form.getHotelName().isEmpty()) {
+			model.addAttribute("message", BIZERR301);
+			return "/hotelitem/regist/hotel_find";
 		} else {
-			if (form.getHotelCode() == null) {
-				form.setHotelCode("*");
+			if (form.getHotelCode().isEmpty()) {
+				form.setHotelCode("");
 			}
-			if (form.getHotelName() == null) {
-				form.setHotelName("*");
+			if (form.getHotelName().isEmpty()) {
+				form.setHotelName("");
 			}
 		}
 
 		List<Hotel> hotelList = service.findHotel(form.getHotelCode(),
 				form.getHotelName());
 
+		if (hotelList.isEmpty()) {
+			model.addAttribute("message", BIZERR302);
+			return "/hotelitem/regist/hotel_find";
+		}
+
 		model.addAttribute("hotelList", hotelList);
 
-		return "V0501_01HotelFindView";
+		return "/hotelitem/regist/hotel_find";
 	}
 
 	// リンクを押されたとき、登録画面に遷移
-	@RequestMapping("/hotelItem/register/confirmHotel/{hotelCode}")
+	@RequestMapping("/register/confirmhotel/{hotelCode}")
 	public String confirmHotel(@PathVariable String hotelCode, Model model) {
+		List<Hotel> hotelList = service.findHotel(hotelCode, "");
 
-		List<Hotel> hotelList = service.findHotel(hotelCode, "*");
+		RegistHotelForm registForm = new RegistHotelForm();
+		registForm.setHotelCode(hotelList.get(0).getHotelCode());
+		registForm.setHotelName(hotelList.get(0).getName());
+		registForm.setCity(hotelList.get(0).getCityName());
+		registForm.setGrade(hotelList.get(0).getGrade());
 
-		model.addAttribute("hotelList", hotelList);
-		return "V0501_02HotelRegistView";
+		model.addAttribute("registHotelForm", registForm);
+
+		// model.addAttribute("hotelList", hotelList);
+		return "/hotelitem/regist/hotel_regist";
 	}
 
 	// 登録ボタンが押されたとき、登録確認画面に遷移
-	@RequestMapping("/hotelItem/register/registHotelItem")
+	@RequestMapping("/register/registhotelitem")
 	public String registHotelItem(@Validated RegistHotelForm form,
 			BindingResult result, Model model) {
 
 		if (result.hasErrors()) {
-			return "V0501_02HotelRegistView";
+			return "/hotelitem/regist/hotel_regist";
 		}
 
+		// ホテルコード、宿泊日、料金が一致している場合同じ画面に戻る
+		int count = service.countHotelItem(form.getHotelCode(), form.getDate(),
+				form.getPrice());
+		if (count != 0) {
+			// TODO(ren): use error code
+			model.addAttribute("message", "同じ商品が登録されています。");
+			return "/hotelitem/regist/hotel_regist";
+		}
+
+		// 自動でformがモデルにaddされるんだっけ？これいらん？最後のlasthotelcodenのaddはいるのか
 		model.addAttribute("registHotelForm", form);
 
 		// 商品コード自動付番
@@ -125,33 +139,33 @@ public class RegistHotelItemController {
 		// str2をint型に変換して+1する
 		str2 = Integer.toString(Integer.parseInt(str2) + 1);
 
+		str2 = String.format("%06d", Integer.parseInt(str2));
+
 		// もう一度HTL******の形に戻す
 		lastHotelCode = str1 + str2;
 
 		model.addAttribute("lastHotelCode", lastHotelCode);
+		form.setHotelItemCode(lastHotelCode);
 
-		return "V0501_03HotelRegistConfirmView.html";
+		return "/hotelitem/regist/hotel_regist_confirm";
 	}
 
 	// 確定ボタンが押されたとき、登録結果画面に遷移
-	@RequestMapping("/hotelItem/register/commitHotelItem")
+	@RequestMapping("/register/commithotelitem")
 	public String commitHotelItem(RegistHotelForm form, Model model) {
 
 		HotelItem hotelitem = new HotelItem();
 		hotelitem.setItemCode((String) model.getAttribute("lastHotelCode"));
-
-		List<Hotel> hotelList = service.findHotel(form.getHotelCode(), "*");
+		List<Hotel> hotelList = service.findHotel(form.getHotelCode(), "");
 		hotelitem.setHotel(hotelList.get(0));
-
 		hotelitem.setDate(form.getDate());
 		hotelitem.setPrice(form.getPrice());
 		hotelitem.setStock(form.getStock());
-
 		service.registHotelItem(hotelitem);
 
 		model.addAttribute("registHotelForm", form);
 
-		return "V0501_04HotelRegistResultView";
+		return "/hotelitem/regist/hotel_regist_result";
 	}
 
 	// bizinessexceptinをキャッチ
@@ -160,9 +174,9 @@ public class RegistHotelItemController {
 		// エラーメッセージをキー名"message"でModelに格納
 		model.addAttribute("message", e.getMessage());
 
-		model.addAttribute("hotelFindForm", new FindHotelForm());
+		model.addAttribute("findHotelForm", new FindHotelForm());
 
-		return "V0501_01HotelFindView";
+		return "/hotelitem/regist/hotel_find";
 	}
 
 }
