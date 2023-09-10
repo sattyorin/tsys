209ff2c6f0de07733c558e3d9d20997ddfa63b5a
s-ozeki <hoge@hoge.com>
2023-09-10 17:18:07 +0900

    sara: set loginMember instead of memberCode

--- src/main/java/jp/co/tsys/common/control/GlobalControllerAdvice.java
+++ src/main/java/jp/co/tsys/common/control/GlobalControllerAdvice.java
@@ -2,38 +2,49 @@ package jp.co.tsys.common.control;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import jp.co.tsys.common.entity.Member;
 import jp.co.tsys.common.exception.UserNameNotFoundException;
 import jp.co.tsys.common.form.LoginForm;
+import jp.co.tsys.common.service.RetrieveMemberService;
 
 /**
  * @author sara
- *
  */
 @ControllerAdvice
 public class GlobalControllerAdvice {
+	@Autowired
+	private RetrieveMemberService service;
+
 	/**
 	 * @param session
 	 *            ControllerAdviceはModel非対応
 	 * @return userName
 	 */
 	@ModelAttribute("userName")
-	public String userName(HttpSession session, LoginForm loginForm) {
-		String userName = (String) session.getAttribute("userName");
+	public Member userName(HttpSession session, LoginForm loginForm) {
+		Member loginMember = (Member) session.getAttribute("loginMember");
 
-		if (loginForm == null && userName == null) {
+		if (loginForm == null && loginMember == null) {
 			// TODO(risa): set erorr code
 			// TODO(sara): Separate errors when the session times out and when
 			// the user has not yet logged in.
 			throw new UserNameNotFoundException("ログインしてください。");
 		}
-		// TODO(sara): get userName insted of MemberCode
-		session.setAttribute("userName", loginForm.getMemberCode());
-		return userName;
+
+		if (loginMember != null) {
+			return loginMember;
+		}
+
+		loginMember = (Member) service.getMember(loginForm.getMemberCode(),
+				loginForm.getPassword());
+		session.setAttribute("loginMember", loginMember);
+		return loginMember;
 	}
 
 	@ExceptionHandler(UserNameNotFoundException.class)
