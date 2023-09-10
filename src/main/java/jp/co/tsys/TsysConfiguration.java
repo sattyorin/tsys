c97ddeb016c314b7330719b349508f1fec809da4
s-ozeki <hoge@hoge.com>
2023-09-10 17:08:43 +0900

    sara: added tsys-data.properties

--- src/main/java/jp/co/tsys/TsysConfiguration.java
+++ src/main/java/jp/co/tsys/TsysConfiguration.java
@@ -1,20 +1,21 @@
 /**
  * TsysConfiguration.java
- * All Rights Reserved, Copyright(c) Fujitsu Learning Media Limited
  */
-
 package jp.co.tsys;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import jp.co.tsys.common.aspect.LoggingAspect;
 
 /**
  * コンフィグレーションクラス
+ *
  * @author FLM
  * @version 1.0.0
  */
@@ -25,19 +26,27 @@ public class TsysConfiguration {
 
 	/**
 	 * ResourceBundleMessageSourceのBean定義
+	 *
 	 * @return {@code ResourceBundleMessageSource}
 	 */
 	@Bean
 	ResourceBundleMessageSource messageSource() {
-		ResourceBundleMessageSource messageSource
-				= new ResourceBundleMessageSource();
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.setDefaultEncoding("UTF-8");
 		messageSource.setBasenames("tsys-messages");
 		return messageSource;
 	}
 
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+		configurer.setLocation(new ClassPathResource("tsys-data.properties"));
+		return configurer;
+	}
+
 	/**
 	 * LoggingAspectクラスのBean定義
+	 *
 	 * @return {@code LoggingAspect}
 	 */
 	@Bean
