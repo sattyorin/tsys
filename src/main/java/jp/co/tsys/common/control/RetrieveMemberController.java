460393c745e8bd39633bfa3b297016660072652b
s-ozeki <hoge@hoge.com>
2023-09-10 17:26:07 +0900

    sara: used session

--- src/main/java/jp/co/tsys/common/control/RetrieveMemberController.java
+++ src/main/java/jp/co/tsys/common/control/RetrieveMemberController.java
@@ -4,34 +4,28 @@
 
 package jp.co.tsys.common.control;
 
-import static jp.co.tsys.common.util.MessageList.BIZERR001;
-
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import jp.co.tsys.common.entity.Member;
 import jp.co.tsys.common.form.LoginForm;
-import jp.co.tsys.common.service.RetrieveMemberService;
 
 /**
  * ログイン・ログアウトController
  */
-@SessionAttributes(names = "loginmember")
 @Controller
 public class RetrieveMemberController {
 
 	/** Service */
-	@Autowired
-	private RetrieveMemberService service;
+	// @Autowired
+	// private RetrieveMemberService service;
 
 	/**
 	 * ログイン画面の【ログイン】に対応するHandlerメソッド マッピングするURL：/retrievemember
@@ -46,30 +40,15 @@ public class RetrieveMemberController {
 	 */
 	@RequestMapping("/retrievemember")
 	public String retrieveMember(@Validated LoginForm loginForm,
-			BindingResult result, Model model) {
+			BindingResult result, Model model, HttpSession session) {
 		// 入力チェック
 		if (result.hasErrors()) {
 			return "/login";
 		}
-		// ServiceのgetMemberメソッドを呼び出し
-		// 戻り値のMemberオブジェクトを取得する
-		Member loginmember = service.getMember(loginForm.getMemberCode(),
-				loginForm.getPassword());
-
-		// Memberオブジェクトをキー名"loginmember"でモデルに格納
-		model.addAttribute("loginmember", loginmember);
-
-		// 該当するメンバー情報が存在しない場合
-		if (loginmember == null) {
-			// エラーメッセージ[BIZERR001]をキー名"message"でModelに格納
-			model.addAttribute("message", BIZERR001);
-
-			// ログイン画面（/login）を返却する
-			return "/login";
-		}
 
 		// 権限によって画面遷移先を振り分ける
-		if (loginmember.getRole().equals("Employee")) {
+		Member loginMember = (Member) session.getAttribute("loginMember");
+		if (loginMember.getRole().equals("Employee")) {
 			// RoleがEmployeeの場合の画面遷移先
 			return "/top_menu_emp";
 		} else {
