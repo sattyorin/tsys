29c30b376a6287ea0b6dab322e3364e8d1c78376
s-ozeki <hoge@hoge.com>
2023-09-11 12:59:18 +0900

    ichinose: modified js

---
+++ src/main/resources/static/js/paging.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#paging').DataTable({
+		// 検索機能 無効
+		searching: false,
+		// 日本語表示
+		"language": {
+			"url": "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
+		}
+	});
+});
