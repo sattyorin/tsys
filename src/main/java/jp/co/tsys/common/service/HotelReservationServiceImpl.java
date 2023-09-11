c86ec102a0e27aea62e25f78ea103108eed41128
s-ozeki <hoge@hoge.com>
2023-09-11 16:18:57 +0900

    risa: modified design

--- src/main/java/jp/co/tsys/common/service/HotelReservationServiceImpl.java
+++ src/main/java/jp/co/tsys/common/service/HotelReservationServiceImpl.java
@@ -4,19 +4,16 @@
 
 package jp.co.tsys.common.service;
 
-import static jp.co.tsys.common.util.MessageList.BIZERR101;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jp.co.tsys.common.entity.HotelItem;
-import jp.co.tsys.common.exception.BusinessException;
 import jp.co.tsys.common.mapper.HotelItemMapper;
 
 /**
  *
  * ホテルの詳細情報を表示するためのService実装クラス
- * 
+ *
  * @author 大久保
  * @version 1.0.0
  */
@@ -30,9 +27,6 @@ public class HotelReservationServiceImpl {
 	public HotelItem findHotelDetail(String itemCode) {
 		HotelItem hotelItem = mapper.findHotelDetail(itemCode);
 
-		if (hotelItem == null) {
-			throw new BusinessException(BIZERR101);
-		}
 		return hotelItem;
 	}
 }
